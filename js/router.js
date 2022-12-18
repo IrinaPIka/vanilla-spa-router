748bc81098d30c97302808980b87d4a7d10b78b2
IrinaPIka <112224011+IrinaPIka@users.noreply.github.com>
2022-12-18 13:25:40 +0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -6,10 +6,10 @@ const route = (event) => {
 };
 
 const routes = {
-    404: "/pages/404.html",
-    "/": "/pages/index.html",
-    "/about": "/pages/about.html",
-    "/lorem": "/pages/lorem.html",
+    404: "vanilla-spa-router/pages/404.html",
+    "/": "vanilla-spa-router/pages/index.html",
+    "/about": "vanilla-spa-router/pages/about.html",
+    "/lorem": "vanilla-spa-router/pages/lorem.html",
 };
 
 const handleLocation = async () => {
